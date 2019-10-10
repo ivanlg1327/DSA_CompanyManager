3d65101f4d1214e53883e0b74406e475e4f7e184
Ivan <ivanl.g.27@hotmail.com>
2019-10-10 13:04:36 +0200

    Basic_structure

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,14 @@
-public class Company {
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+public class Company implements Comparator {
+    String companyName;
+    String description;
+    List <Employee> employeeList;
+    @Override
+    public int compare(Object o, Object t1) {
+        return 0;
+    }
+
+
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,8 @@
+import java.util.Date;
 public class Employee {
+    String name;
+    String surname;
+    Date birthday;
+    double salary;
+    String companyName;
 }
