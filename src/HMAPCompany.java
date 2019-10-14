a397ed88de21d1a390192f33676e2db9a320b381
Ivan <ivanl.g.27@hotmail.com>
2019-10-14 09:28:45 +0200

    Basic_structure

--- src/HMAPCompany.java
+++ src/HMAPCompany.java
@@ -4,6 +4,25 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class HMAPCompany {
+
     HashMap<String, Company> Companies = new HashMap<>();
+    public HMAPCompany(HashMap<String, Company> companies) {
+        Companies = companies;
+    }
+    private void putCompany(String Id, Company comp){
+        Companies.put(Id,comp);
+    }
+    private Company getCompany(String Id){
+        return Companies.get(Id);
+    }
+
+    private Company deleteCompany(String Id){
+        return Companies.remove(Id);
+    }
+
+    private int companyCount(){
+        return Companies.size();
+    }
+
     // CRUD
 }
