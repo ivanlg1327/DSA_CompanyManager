d67a0740d205307d76d4e6eadb00dadfe9c51816
Ivan <ivanl.g.27@hotmail.com>
2019-10-15 11:43:10 +0200

    Comentarios sobre el logger

--- src/main/java/HMAPCompany.java
+++ src/main/java/HMAPCompany.java
@@ -1,27 +1,28 @@
 import java.util.*;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Level;
-import org.apache.log4j.spi.LoggingEvent;
+
 
 public class HMAPCompany {
-// lo del log y el logger lo ha puesto el profe aun tengo que ver como funciona y ponerlo bien
+
     HashMap<String, Company> companies = new HashMap<String, Company>();
-    private final List<LoggingEvent> log = new ArrayList<LoggingEvent>();
+
+    public Logger log;
     public HMAPCompany(HashMap<String, Company> compa) {
         companies = compa;
     }
     private void putCompany(String id, Company comp){
-     //   log.info("nova companyia: "+comp);
+        log.info("nova companyia: "+comp);
+
         companies.put(id,comp);
     }
     private Company getCompany(String id) {
         Company company = companies.get(id);
-        //log.info("retornem: "+ company);
-        // if () {
-        // log.error // log.warn(" xxx" );
-        // }
+        log.info("retornem: "+ company);
+        int i=0;
+        if (i==1) {
+          log.warn(" xxx" );
+         }
 
         return company;
     }
